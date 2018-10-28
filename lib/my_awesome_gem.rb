90db706e7447b0a181b83fc8ab03a33469891537
Yurii Halapup <halapup.yurii@gmail.com>
2018-10-28 14:54:11 +0200

    add my_awesome_gem/my_awesome_gem.rb and require it in lib/my_awesome_gem.rb

--- lib/my_awesome_gem.rb
+++ lib/my_awesome_gem.rb
@@ -1,4 +1,5 @@
 require "my_awesome_gem/version"
+require "my_awesome_gem/my_awesome_gem"
 
 module MyAwesomeGem
   class Error < StandardError; end
---
+++ lib/my_awesome_gem/my_awesome_gem.rb
@@ -0,0 +1,12 @@
+module MyAwesomeGem
+  class Whois
+    def self.awesome?
+      puts "you are awesome"
+      true
+    end
+
+    def say_hello
+      puts "say hello"
+    end
+  end
+end
